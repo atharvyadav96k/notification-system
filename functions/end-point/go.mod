26a09a12d50d0ed0bb9cccdc0c643611876d7adb
Atharv Yadav <atharv.yadav@xperate.com>
2026-08-23 23:52:44 +0530

    Refactor notification sending to use AWS SQS instead of RabbitMQ
    - Removed RabbitMQ dependencies and connection setup.
    - Integrated AWS SDK for SQS to send notifications.
    - Updated the SendNotification function to marshal the notification and send it to the SQS queue.
    - Added environment variable support for SQS queue URL.

--- functions/end-point/go.mod
+++ functions/end-point/go.mod
@@ -3,16 +3,23 @@ module github.com/atharvyadav96k/notification-system/function/end-point
 go 1.25.5
 
 require (
-	github.com/aws/aws-lambda-go v1.54.0 // indirect
-	github.com/cloudevents/sdk-go v1.2.0 // indirect
-	github.com/google/uuid v1.1.1 // indirect
-	github.com/hashicorp/golang-lru v0.5.1 // indirect
-	github.com/lightstep/tracecontext.go v0.0.0-20181129014701-1757c391b1ac // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	github.com/rabbitmq/amqp091-go v1.14.0 // indirect
-	go.opencensus.io v0.22.0 // indirect
-	go.uber.org/atomic v1.4.0 // indirect
-	go.uber.org/multierr v1.1.0 // indirect
-	go.uber.org/zap v1.10.0 // indirect
-	pack.ag/amqp v0.11.0 // indirect
+	github.com/aws/aws-lambda-go v1.54.0
+	github.com/aws/aws-sdk-go-v2 v1.43.7
+	github.com/aws/aws-sdk-go-v2/config v1.32.38
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.46.7
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.19.37 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.38 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.38 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.38 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.39 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.38 // indirect
+	github.com/aws/aws-sdk-go-v2/service/signin v1.5.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.33.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.38.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.45.7 // indirect
+	github.com/aws/smithy-go v1.27.8 // indirect
 )
--- functions/end-point/helpers/handleRequest.go
+++ functions/end-point/helpers/handleRequest.go
@@ -3,19 +3,24 @@ package helpers
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"os"
 
 	"github.com/atharvyadav96k/notification-system/function/end-point/applayer"
-	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
 )
 
-var conn *amqp.Connection
-var ch *amqp.Channel
-
-const queueName = "notification"
+var sqsClient *sqs.Client
+var queueURL string
 
 func init() {
-	// queue connection
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	sqsClient = sqs.NewFromConfig(cfg)
+	queueURL = os.Getenv("SQS_QUEUE_URL")
 }
 
 func SendNotification(ctx context.Context, event json.RawMessage) error {
@@ -23,30 +28,18 @@ func SendNotification(ctx context.Context, event json.RawMessage) error {
 	if err := json.Unmarshal(event, &notification); err != nil {
 		return err
 	}
-	err := putMessageInTheQueue(notification)
-	if err != nil {
-		fmt.Println("Failed to send the message")
-	}
-	return nil
+	return putMessageInTheQueue(ctx, notification)
 }
 
-func putMessageInTheQueue(notification applayer.Message) error {
-	_, err := json.Marshal(notification)
+func putMessageInTheQueue(ctx context.Context, notification applayer.Message) error {
+	body, err := json.Marshal(notification)
 	if err != nil {
 		return err
 	}
-	return nil
-}
 
-func publishMessageToRabbitMQ(PRIORITY string, message []byte) error {
-	return ch.Publish(
-		"",
-		PRIORITY,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType: "application/json",
-			Body:        message,
-		},
-	)
+	_, err = sqsClient.SendMessage(ctx, &sqs.SendMessageInput{
+		QueueUrl:    aws.String(queueURL),
+		MessageBody: aws.String(string(body)),
+	})
+	return err
 }
