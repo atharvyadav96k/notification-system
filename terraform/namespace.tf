12851d4d5c547fdc0ba6fee2c70033b72717388c
Atharv Yadav <atharv.yadav@xperate.com>
2026-08-23 22:48:24 +0530

    chore: remove unused Terraform files and configurations for Docker and KEDA

--- terraform/namespace.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_namespace_v1" "notification_system" {
-  metadata {
-    name = var.namespace
-  }
-}
--- terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "worker_image" {
-  description = "Image pushed to the local registry and deployed to the worker Deployment"
-  value       = docker_image.worker.name
-}
