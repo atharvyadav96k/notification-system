21f7e7346e8e9dc7c1144d7f4a1c3f93e89920b3
Atharv Yadav <atharv.yadav@xperate.com>
2026-08-23 22:41:05 +0530

    refactor: simplify main function and remove unused code in notification handling

--- functions/end-point/function.go
+++ functions/end-point/function.go
@@ -1,64 +1,10 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"math/rand"
-	"sync"
-	"time"
-
 	"github.com/atharvyadav96k/notification-system/function/end-point/helpers"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-const (
-	totalRequests = 1_000_000
-	duration      = time.Minute
-)
-
-var notifications = []string{
-	`{
-		"Message": "Your OTP is 123456",
-		"MessageType": 0,
-		"ReceiverAddress": "+919876543210"
-	}`,
-	`{
-		"Message": "Your payment was successful",
-		"MessageType": 1,
-		"ReceiverAddress": "+919876543210"
-	}`,
-	`{
-		"Message": "Redeem offer",
-		"MessageType": 2,
-		"ReceiverAddress": "+919876543210"
-	}`,
-}
-
 func main() {
-	aws := true
-
-	if aws {
-		lambda.Start(helpers.SendNotification)
-	} else {
-		interval := duration / time.Duration(totalRequests)
-		ticker := time.NewTicker(interval)
-		defer ticker.Stop()
-
-		var wg sync.WaitGroup
-
-		for i := 0; i < totalRequests; i++ {
-			<-ticker.C
-
-			notification := notifications[rand.Intn(len(notifications))]
-			event := json.RawMessage(notification)
-
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				helpers.SendNotification(context.Background(), event)
-			}()
-		}
-
-		wg.Wait()
-	}
+	lambda.Start(helpers.SendNotification)
 }
--- functions/end-point/helpers/handleRequest.go
+++ functions/end-point/helpers/handleRequest.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 
 	"github.com/atharvyadav96k/notification-system/function/end-point/applayer"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -16,30 +15,7 @@ var ch *amqp.Channel
 const queueName = "notification"
 
 func init() {
-	var err error
-	conn, err = amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ch, err = conn.Channel()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = ch.QueueDeclare(
-		queueName,
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// queue connection
 }
 
 func SendNotification(ctx context.Context, event json.RawMessage) error {
@@ -55,14 +31,14 @@ func SendNotification(ctx context.Context, event json.RawMessage) error {
 }
 
 func putMessageInTheQueue(notification applayer.Message) error {
-	message, err := json.Marshal(notification)
+	_, err := json.Marshal(notification)
 	if err != nil {
 		return err
 	}
-	return publishMessage(queueName, message)
+	return nil
 }
 
-func publishMessage(PRIORITY string, message []byte) error {
+func publishMessageToRabbitMQ(PRIORITY string, message []byte) error {
 	return ch.Publish(
 		"",
 		PRIORITY,
