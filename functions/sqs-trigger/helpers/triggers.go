bd01a14496c55d7891947926ea581bafda5a43fb
Atharv Yadav <atharv.yadav@xperate.com>
2026-08-25 01:10:02 +0530

    fix: update Trigger function to print alarm message and return error

--- functions/sqs-trigger/helpers/triggers.go
+++ functions/sqs-trigger/helpers/triggers.go
@@ -2,7 +2,7 @@ package helpers
 
 import "fmt"
 
-func Trigger() string {
-	fmt.Println("Hello World")
-	return "Hello World"
+func Trigger() error {
+	fmt.Println("Alarms Triggered")
+	return nil
 }
