764eb9a3818a27416d20165cec61643960cf0122
Atharv Yadav <atharv.yadav@xperate.com>
2026-08-29 14:41:25 +0530

    feat: add AWS security group and update worker instance configuration

--- workers/consumer/terraform.tf
+++ workers/consumer/terraform.tf
@@ -1,3 +1,5 @@
+provider "aws" {}
+
 terraform {
   backend "s3" {}
 }
@@ -6,11 +8,31 @@ variable "region" {
   type = string
 }
 
-resource "aws_instance" "ubentu"{
+resource "aws_security_group" "worker-security" {
+  name = "notification-worker-sg"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "worker"{
     ami = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
     instance_type = "t3.micro"
-    region = var.region
 
+    vpc_security_group_ids = [aws_security_group.worker-security.id]
     user_data = <<-EOF
     #!/bin/bash
     echo "install go"
