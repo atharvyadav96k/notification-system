e938ba5b6a4f79dcce3094fba2479ea6b7f12cab
Atharv Yadav <atharv.yadav@xperate.com>
2026-08-30 13:35:40 +0530

    feat: add IAM policy and role attachment for EC2 instance management in Lambda function

--- functions/sqs-trigger/terraform.tf
+++ functions/sqs-trigger/terraform.tf
@@ -29,6 +29,31 @@ resource "aws_s3_object" "function_zip" {
   etag   = filemd5("${path.module}/function.zip")
 }
 
+locals {
+  lambda_role_name = element(split("/", var.lambda_role_arn), length(split("/", var.lambda_role_arn)) - 1)
+}
+
+resource "aws_iam_policy" "lambda_ec2_start_policy" {
+  name        = "${var.function-name}-ec2-start-policy"
+  description = "Allows Lambda to start the worker EC2 instance"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["ec2:DescribeInstances", "ec2:StartInstances"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ec2_start_policy" {
+  role       = local.lambda_role_name
+  policy_arn = aws_iam_policy.lambda_ec2_start_policy.arn
+}
+
 resource "aws_lambda_permission" "function_url_public_access" {
   statement_id           = "AllowPublicFunctionUrlInvoke"
   action                 = "lambda:InvokeFunctionUrl"
@@ -61,6 +86,7 @@ resource "aws_lambda_function" "notification_publisher" {
   source_code_hash = filebase64sha256("${path.module}/function.zip")
   
   depends_on = [
-    aws_s3_object.function_zip
+    aws_s3_object.function_zip,
+    aws_iam_role_policy_attachment.attach_ec2_start_policy
   ]
 }
